f37193126ddbd26fdf57fdc21fe6b0a828d1fc22
pmagalhaes2 <paath.magalhaes2@gmail.com>
2024-10-18 12:39:50 -0300

    chore: JPA repository, dependency injection, database seeding

--- src/main/java/com/example/order_management_system/resources/UserResource.java
+++ src/main/java/com/example/order_management_system/resources/UserResource.java
@@ -1,19 +1,26 @@
 package com.example.order_management_system.resources;
 
 import com.example.order_management_system.entities.User;
+import com.example.order_management_system.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserResource {
+
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@email.com", "11978456213", "abc123");
-        return ResponseEntity.status(HttpStatus.CREATED).body(u);
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.findAll());
     }
 
 }
